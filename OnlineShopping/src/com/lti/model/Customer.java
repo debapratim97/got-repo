57373587fa0fad1868b41fef50a480e5867138bc
HP <HP@DESKTOP-DEH4JOP>
2020-10-15 15:52:02 +0530

    A variable is added

--- OnlineShopping/src/com/lti/model/Customer.java
+++ OnlineShopping/src/com/lti/model/Customer.java
@@ -3,6 +3,13 @@ package com.lti.model;
 public class Customer {
 	int custId;
 	String custName;
+	String customerCity;
+	public String getCustomerCity() {
+		return customerCity;
+	}
+	public void setCustomerCity(String customerCity) {
+		this.customerCity = customerCity;
+	}
 	public int getCustId() {
 		return custId;
 	}
