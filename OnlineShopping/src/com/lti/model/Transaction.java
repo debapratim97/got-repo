56d898d0ec561a799108fca5b3ba4e8d5a49e2f5
HP <HP@DESKTOP-DEH4JOP>
2020-10-16 09:35:36 +0530

    added Transaction through master.

---
+++ OnlineShopping/src/com/lti/model/Transaction.java
@@ -0,0 +1,29 @@
+package com.lti.model;
+
+import java.time.LocalDate;
+
+public class Transaction {
+	int transactionId;
+	int amount;
+	LocalDate transactionDate;
+	public int getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(int transactionId) {
+		this.transactionId = transactionId;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public LocalDate getTransactionDate() {
+		return transactionDate;
+	}
+	public void setTransactionDate(LocalDate transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+	
+	
+}
